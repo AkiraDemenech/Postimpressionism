0a2ac0c7649d734454a682d310723210167ae670
AkiraDemenech <guiakira.demenechmori@gmail.com>
2021-02-03 19:23:19 -0300

    Aquele famoso OverFlow e um print esquisito de volta ao normal
    muitas linhas muitas linhas
    menos pixel

--- beforeThePackage/Red.py
+++ beforeThePackage/Red.py
@@ -4,8 +4,8 @@ import random
 from numpy import empty, uint8
 
 	
-ln = 768
-col = 1366
+ln = 78
+col = 131
 delta = 16
 output = 'GERAçãO.pNG'
 redonly = empty((ln,col,3),uint8)
@@ -20,7 +20,7 @@ for y in range(ln):
 		if x:
 			red = redonly[y,x-1,0]
 			if y:
-				red = (red + redonly[y-1,x,0] + redonly[y-1,x-1,0])/3
+				red = (red/3 + redonly[y-1,x,0]/3 + redonly[y-1,x-1,0]/3)
 		elif y:
 			red = redonly[y-1,x,0]
 		red = variar(red)
@@ -33,7 +33,7 @@ for y in range(ln):
 			redlike[y,x,2] = uint8(red*random.random())	
 
 		
-	print(y,end='')
+	print(y)
 
 
 		
